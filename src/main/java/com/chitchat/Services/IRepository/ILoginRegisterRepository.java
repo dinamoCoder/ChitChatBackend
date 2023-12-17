74a87dd5b47b9fb3e758bd546163f29e3c354aab
suraj gusain <surajgusain2412@gmail.com>
2023-12-17 13:01:49 +0530

    do some change

--- src/main/java/com/chitchat/Services/IRepository/ILoginRegisterRepository.java
+++ src/main/java/com/chitchat/Services/IRepository/ILoginRegisterRepository.java
@@ -1,12 +1,9 @@
 package com.chitchat.Services.IRepository;
 
-import java.util.Optional;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
 import com.chitchat.Models.User;
-import java.util.List;
 
 
 
@@ -14,7 +11,7 @@ import java.util.List;
 public interface ILoginRegisterRepository<T> extends MongoRepository<User,String>  {  
     @Query("{ 'userName' : ?0 }")
     User findByUserName(String userName);
-    User findByEmail(String Email);
+   // User findByEmail(String Email);
     @Query("{ '_id' : ?0 }")
     User findByuserId(String userId);
 }
